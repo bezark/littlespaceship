5c1c87fd571d1acdf43990f9e476a5d06881785a
Glitch (hello-express) <none>
2019-04-20 19:34:37 +0000

    ⛪️⛰ Checkpoint ./server.js:3725709/797

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+// io.set('transports', [ 'websocket' ]);
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 io.on('connection', function (socket){
    console.log('connection with '+socket.id);
-// console.log(socket);
+console.log(socket);
+    io.sockets.emit('hello', 'hellomax');
  
 
   socket.on('CH', function (from, msg) {
     console.log('MSG', from, ' saying ', msg);
-      io.sockets.emit('hello', 'hellomax');
   });
 
   
