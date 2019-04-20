6abae3e156bdc0847d16ceaa59e37229c2257afd
Glitch (hello-express) <none>
2019-04-20 19:23:35 +0000

    👨‍❤️‍👨🚣 Checkpoint ./server.js:3725709/209

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', function (socket){
-   console.log('connection with'+socket.id);
-  console.log(socket);
+   console.log('connection with '+socket.id);
+// console.log(socket);
  
 
-  socket.on('CH01', function (from, msg) {
+  socket.on('CH', function (from, msg) {
     console.log('MSG', from, ' saying ', msg);
+      io.sockets.emit('hello', 'hellomax');
   });
 
+  
+  
+  socket.on('hey', function (from, msg) {
+    console.log('MSG', from, ' saying ', msg);
+  });
 });
 
 http.listen(4000, function () {
