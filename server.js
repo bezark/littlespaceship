9c7cbb8435151362fa3b3264a485a15f9017a55c
Glitch (hello-express) <none>
2019-04-20 19:01:47 +0000

    🍲🎻 Checkpoint ./server.js:3725709/2424

--- server.js
+++ server.js
@@ -1,43 +1,17 @@
-// server.js
-// where your node app starts
+//server.js
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-// init project
-const express = require('express');
-const app = express();
+io.on('connection', function (socket){
+   console.log('connection');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+  socket.on('CH01', function (from, msg) {
+    console.log('MSG', from, ' saying ', msg);
+  });
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+http.listen(4000, function () {
+  console.log('listening on *:3000');
 });
-
-
-
-const server = require('http').createServer();
-
-const io = require('socket.io')(server, {
-  path: '/test',
-  serveClient: false,
-  // below are engine.IO options
-  pingInterval: 10000,
-  pingTimeout: 5000,
-  cookie: false
-});
-
-server.listen(4000);
-console.log('Your app is listening on port 4000') 
-
-
-
-io.on("connection", function(socket) {
-console.log("new connection: "+socket.id);})
