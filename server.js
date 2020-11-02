6e1b68b48ca34f711d3154a1f03149d6ae6bbc91
bezark <johnbezark@gmail.com>
2020-11-01 22:29:37 -0500

    msg was not definted

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ http.listen(process.env.PORT || 3000, process.env.IP, () => {
 
 io.sockets.on('connection', socket => {
   socket.on('msg', data => {
-    console.log(msg, data)
+    console.log(data)
     socket.broadcast.emit('msg', data);
   });
 });
