e6802de82ea2c076ca127d5ceddbb1b6541073f5
David Currie <dac@dac.lan>
2020-11-06 09:38:45 -0500

    little space ship game

--- public/sketch.js
+++ public/sketch.js
@@ -287,11 +287,12 @@ function title() {
     fill(0);
     rect(0, 0, width, height);
     noStroke();
-    textSize(30);
+    textSize(50);
     textAlign(CENTER, CENTER);
     fill(100, 255, 255);
 
-    text('LITTLE SPACE SHIP', width / 2, height / 2);
+    text('LITTLE SPACE SHIP GAME', width / 2, height / 2);
+    textSize(30);
     text('Click to Play', width / 2, height / 2 + 40);
     if (mouseIsPressed) {
       titleScreen = false;
